9951b76c99a4163dcc1df261cfda394b31f0ab0e
Tomomi_Murakami <60651065+sleepingdog0529@users.noreply.github.com>
2021-11-04 23:53:14 +0900

    エントリポイントを微修正

--- WX2Engine/core/entry_point.h
+++ WX2Engine/core/entry_point.h
@@ -17,11 +17,17 @@ INT WINAPI WinMain(
 	_In_ HINSTANCE hInstance, 
 	_In_opt_ HINSTANCE hPrevInstance, 
 	_In_ LPSTR lpCmdLine,
-	_In_ int nShowCmd )
+	_In_ int nShowCmd)
+{
+	UNREFERENCED_PARAMETER(hInstance);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(nShowCmd);
 #else
 int main(int argc, char** argv)
-#endif
 {
+#endif
+
 	auto app = wx2::CreateApp();
 
 	app->Run();
